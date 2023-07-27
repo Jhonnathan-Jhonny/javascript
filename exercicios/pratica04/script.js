e6485cbfefd7ef7e67d7fc0e8418ad767a445dad
Jhonnathan-Jhonny <jhonnathan.rodrigues@aluno.uepb.edu.br>
2023-07-27 18:28:33 -0300

    tirando imagens e criando pratica04

--- exercicios/pratica04/script.js
+++ exercicios/pratica04/script.js
@@ -1,21 +1,14 @@
-function carregar(){
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
-
-
-    if(hora > 0 && hora < 13){
-        img.src = "imagens/manha.png"
-        document.body.style.backgroundColor = '#e2cd9f'
-    } 
-    else if (hora >= 13 && hora < 18){
-        img.src = "imagens/tarde.png"
-        document.body.style.backgroundColor = '#b9846f'
-    } 
-    else {
-        img.src = "imagens/noite.png"
-        document.body.style.backgroundColor = '#515154'
+function gerar(){
+    var num = document.getElementById('num').value
+    var res =  document.getElementById('res')
+    res.innerHTML = ''
+    if(num.length == 0){
+        window.alert('Digite um valor')
+        return
+    }
+    for (var i = 0; i<=10; i++){
+        var item = document.createElement('option')
+        item.text = `${i} x ${num} = ${num*i}`
+        res.appendChild(item)
     }
 }
